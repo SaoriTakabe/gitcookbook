f630226b5cd64761a6145e3a8b9526cd5950eb0c
SHL <lastgeneral@naver.com>
2018-11-11 07:21:48 +0000

    page234

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,19 @@
 node 'cookbook' {
-  file {'/tmp/hello':
-    content => "Hello, world SH\n",
+  class { 'heartbeat::vip':
+    ip1 => '10.0.2.4',
+    ip2 => '10.0.2.5',
+    node1 => 'cookbook',
+    node2 => 'cookbook2',
+    vip => '10.0.2.6/24',
   }
-  include puppet
+}
+
+node 'cookbook2' {
+  class { 'heartbeat::vip':
+    ip1 => '10.0.2.4',
+    ip2 => '10.0.2.5',
+    node1 => 'cookbook',
+    node2 => 'cookbook2',
+    vip => '10.0.2.6/24',
+  }
 }
